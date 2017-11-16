b17391105671613562a555c44b56be0c1741b1c5
Waldemar Just <Waldemarjust@gmx.de>
2017-11-16 03:22:11 +0100

    [ADDED] BP_LevelStart_Interface [UPDATED] All Items for new Vision

--- Source/The_Vision/BeginLevel_Interface.cpp
+++ Source/The_Vision/BeginLevel_Interface.cpp
@@ -3,8 +3,14 @@
 #include "The_Vision.h"
 #include "BeginLevel_Interface.h"
 
+// This function does not need to be modified.
+UBeginLevel_Interface::UBeginLevel_Interface(const class FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+}
 
 // Add default functionality here for any IBeginLevel_Interface functions that are not pure virtual.
+
 
 void IBeginLevel_Interface::BeginLevel()
 {
